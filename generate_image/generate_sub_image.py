44270c51dae8274b9f93a76647656e9cdebd62a4
sy-app <seijuslp4@icloud.com>
2021-07-10 02:32:18 +0900

    フッター画像のユーザ名とハッシュタグの'LLNow'の位置が揃うように調整

--- generate_image/generate_sub_image.py
+++ generate_image/generate_sub_image.py
@@ -18,9 +18,11 @@ def generate_sub_image(img_config):
     text_above_twt_w = ' Tweets with '
     text_above_hashtag = '#lovelive'
     text_above_in = ' in'
-    text_below = '{}-{}'.format(since, until)
-    text_right_above = '@LLNow_JP'
-    text_right_below = '#LLNow'
+    text_below = '{}-{} JST'.format(since, until)
+    text_right_at = '@'
+    text_right_username = 'LLNow_JP'
+    text_right_hash = '#'
+    text_right_hashtag_body = 'LLNow'
 
     # 画像サイズ，マージンを取得
     img_width = img_config['img_width']
@@ -39,11 +41,17 @@ def generate_sub_image(img_config):
     font_below = ImageFont.truetype(font_path, 100)
     font_right = ImageFont.truetype(font_path, int((img_height - 10) / 2))
 
-    # 描画
+    # フォントサイズを取得
     width_above_gen_from = font_above.getsize(text_above_gen_from)[0]
     width_above_n_tweet = font_above.getsize(text_above_n_tweet)[0]
     width_above_twt_w = font_above.getsize(text_above_twt_w)[0]
     width_above_hashtag = font_above.getsize(text_above_hashtag)[0]
+    width_right_at = font_right.getsize(text_right_at)[0]
+    width_right_username = font_right.getsize(text_right_username)[0]
+    width_right_hash = font_right.getsize(text_right_hash)[0]
+    width_right_hashtag_body = font_right.getsize(text_right_hashtag_body)[0]
+
+    # 描画
     draw.text((side_margin, 0),
               text_above_gen_from, font=font_above, fill='#333333')
     draw.text((side_margin + width_above_gen_from, 0),
@@ -55,12 +63,14 @@ def generate_sub_image(img_config):
     draw.text((side_margin + width_above_gen_from + width_above_n_tweet + width_above_twt_w + width_above_hashtag, 0),
               text_above_in, font=font_above, fill='#333333')
     draw.text((side_margin + tweet_range_left_margin, 60), text_below, font=font_below, fill='#000')
-    width_right_above = font_right.getsize(text_right_above)[0]
-    width_right_below = font_right.getsize(text_right_below)[0]
-    draw.text((img_width - max(width_right_above, width_right_below) - side_margin, 0),
-              text_right_above, font=font_right, fill='#000')
-    draw.text((img_width - max(width_right_above, width_right_below) - side_margin, (img_height - 10) / 2 + 5),
-              text_right_below, font=font_right, fill='#1DA1F2')
+    draw.text((img_width - max(width_right_username, width_right_hashtag_body) - width_right_at - side_margin, 0),
+              text_right_at, font=font_right, fill='#000')
+    draw.text((img_width - max(width_right_username, width_right_hashtag_body) - side_margin, 0),
+              text_right_username, font=font_right, fill='#000')
+    draw.text((img_width - max(width_right_username, width_right_hashtag_body) - width_right_hash - side_margin, (img_height - 10) / 2 + 5),
+              text_right_hash, font=font_right, fill='#1DA1F2')
+    draw.text((img_width - max(width_right_username, width_right_hashtag_body) - side_margin, (img_height - 10) / 2 + 5),
+              text_right_hashtag_body, font=font_right, fill='#1DA1F2')
 
     # 画像を保存
     img.save('/tmp/sub_image.png', quality=100, dpi=(600, 600), optimize=True)
