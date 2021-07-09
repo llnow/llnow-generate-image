01de309d979324e77ba4917e8254b1a6499cf36a
sy-app <seijuslp4@icloud.com>
2021-07-10 01:23:10 +0900

    stopwordsとcolormapをdynamodbから取得する

--- generate_image/generate_wc.py
+++ generate_image/generate_wc.py
@@ -11,20 +11,35 @@ def generate_wc(words, img_config, bucket):
     font_path = '/tmp/ヒラギノ角ゴシック W6.ttc'
     bucket.download_file('fonts/ヒラギノ角ゴシック W6.ttc', font_path)
 
+    # dynamodbからstopwordsを取得
+    table = boto3.resource('dynamodb').Table('ll_now')
+    primary_key = {'primary': 'wc_stop_word'}
+    res = table.get_item(Key=primary_key)
+    basic_word = res['Item']['word']['basic_word']
+    lovelive_basic_word = res['Item']['word']['lovelive_basic_word']
+    stopwords = set(basic_word + lovelive_basic_word)
+
+    # dynamodbからcolormap_listを取得
+    primary_key = {'primary': 'colormap_list'}
+    res = table.get_item(Key=primary_key)
+    colormap_list = res['Item']['colormap']
+
+    # colormapをランダムに決定
+    colormap = random.choice(colormap_list)
+
     # wordcloudのサイズを指定
     width = img_config['img_width']
     height = img_config['wc_height']
 
-    colormap_list = ['viridis', 'plasma', 'inferno', 'magma', 'cividis', 'spring', 'summer', 'autumn', 'winter', 'cool', 'ocean', 'gist_earth']
-    wc = wordcloud.WordCloud(font_path=font_path,
-                             background_color='white',
-                             # mask=msk,
-                             colormap=random.choice(colormap_list),
-                             stopwords={"もの", "これ", "ため", "それ", "ところ", "よう", "こと",
-                                        "そう", "ます", "ので", "から", "など", "です", "する", "いる", "ない",
-                                        "あり", "なく", "また", "その", "ある", "なっ", "てる", "この", "なり", "あれ",
-                                        'ちゃん', '位', '日', '発売', '私', 'ん', 'さん', 'RT', '好き', 'の', '定期', 'フォロー'},
-                             width=width, height=height)
+    wc = wordcloud.WordCloud(
+        font_path=font_path,
+        background_color='white',
+        # mask=msk,
+        colormap=colormap,
+        stopwords=stopwords,
+        width=width,
+        height=height
+    )
     wc.generate(' '.join(words))
 
     wc.to_file('/tmp/wc.png')
