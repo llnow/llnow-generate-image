35d39052d9e8495121db19a727644812d6f5a215
sy-app <seijuslp4@icloud.com>
2021-11-21 05:49:15 +0900

    tweets_featuresをDynamoDBではなくssmパラメータストアから取得

--- app/generate_post_image.py
+++ app/generate_post_image.py
@@ -3,7 +3,7 @@ from generate_sub_image import *
 from merge_image import *
 
 
-def generate_post_image(words, img_config, bucket):
+def generate_post_image(words, img_config, bucket, mode):
     generate_wc(words, img_config, bucket)
-    generate_sub_image(img_config)
+    generate_sub_image(img_config, mode)
     merge_image()
