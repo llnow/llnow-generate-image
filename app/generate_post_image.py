0588c2e3d0b22347aaa97ad98f4938d14b8bceea
sy-app <seijuslp4@icloud.com>
2021-12-05 15:33:33 +0900

    条件分岐を修正

--- app/generate_post_image.py
+++ app/generate_post_image.py
@@ -7,9 +7,12 @@ from merge_image import *
 
 def generate_post_image(words, img_config, bucket, mode):
     flag_birthday, birthday_character = check_birthday(mode)
-    mask_wc_on_birthday = birthday_character['mask_wc_on_birthday']
-    if flag_birthday and mask_wc_on_birthday:
-        generate_masked_wc(words, bucket, birthday_character)
+    if flag_birthday:
+        mask_wc_on_birthday = birthday_character['mask_wc_on_birthday']
+        if mask_wc_on_birthday:
+            generate_masked_wc(words, bucket, birthday_character)
+        else:
+            generate_wc(words, img_config, bucket)
     else:
         generate_wc(words, img_config, bucket)
     generate_sub_image(img_config, mode)
