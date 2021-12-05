079278ce4223a0b9060550ace2544b063a1862a3
sy-app <seijuslp4@icloud.com>
2021-12-05 15:18:41 +0900

    誕生日にwordcloudをマスクするかどうかのフラグを確認する

--- app/generate_post_image.py
+++ app/generate_post_image.py
@@ -7,7 +7,8 @@ from merge_image import *
 
 def generate_post_image(words, img_config, bucket, mode):
     flag_birthday, birthday_character = check_birthday(mode)
-    if flag_birthday:
+    mask_wc_on_birthday = birthday_character['mask_wc_on_birthday']
+    if flag_birthday and mask_wc_on_birthday:
         generate_masked_wc(words, bucket, birthday_character)
     else:
         generate_wc(words, img_config, bucket)
